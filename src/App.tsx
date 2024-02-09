421289ee9cb6fdb3ed3cfe91db92dd9d2a55038c
monir178 <monir.mzs17@gmail.com>
2024-02-09 18:43:38 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>This is app</h1>
+      <Button variant="destructive">Click here</Button>
+    </>
+  );
+}
+
+export default App;
